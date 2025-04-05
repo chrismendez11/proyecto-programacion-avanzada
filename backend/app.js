a4d3f3739216ebc40c04c996e76bba5ec29296bd
Christian Mendez <christianmndz.dev@gmail.com>
2025-04-04 20:05:09 -0600

    feat: enhance CORS configuration to allow all origins and specify allowed methods and headers

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,11 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
