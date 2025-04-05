350090c9195c2e8833914aacc4a91f71ca2a1025
Christian Mendez <christianmndz.dev@gmail.com>
2025-04-04 20:29:26 -0600

    refactor: update handleMarkAsDone and fetchHabitsThunk to include token, remove unused variables and inputs

--- frontend/src/app/habits.tsx
+++ frontend/src/app/habits.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { fetchAddHabitThunk, fetchHabitsThunk, markAsDoneThunk } from "@/features/habitSlice";
 import { AppDispatch, RootState } from "@/store/store";
 import { useDispatch, useSelector } from "react-redux";
@@ -16,35 +17,25 @@ type HabitsProps = {
     habits: Habit[];
 }
 
-const handleMarkAsDone = async (habitId: string, dispatch: AppDispatch) => {
-    dispatch(markAsDoneThunk(habitId));
-    dispatch(fetchHabitsThunk());
+const handleMarkAsDone = async (habitId: string, token: string, dispatch: AppDispatch) => {
+    dispatch(markAsDoneThunk({ habitId, token }));
+    dispatch(fetchHabitsThunk(token));
 }
 
 export default function Habits({ habits }: HabitsProps) {
     console.log('habits', habits);
     const dispatch = useDispatch();
-    const status = useSelector((state: RootState) => state.habit.status);
-    const error = useSelector((state: RootState) => state.habit.error);
 
     const calculateProgress = (days: number): number => {
         return Math.min((days / 66) * 100, 100);
     }
 
     const handleAddHabit = () => {
-        if (title && description) {
-            dispatch(fetchAddHabitThunk({ token: user ? user.toString() : '', title, description }));
-            setTitle('');
-            setDescription('');
-            dispatch(fetchHabitsThunk());
-        }
     }
 
     return (
         <div>
             <h1>Habits</h1>
-            <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
-            <input type="text" value={description} onChange={(e) => setDescription(e.target.value)} />
             <button onClick={handleAddHabit}>Add Habit</button>
         </div>
     );
